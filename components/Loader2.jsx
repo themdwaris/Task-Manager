b807404ab082bb11da9030f52c380382b1ca9f1b
Mohammad Waris <31559101+themdwaris@users.noreply.github.com>
2025-05-06 19:34:12 +0530

    Update Loader2.jsx

--- components/Loader2.jsx
+++ components/Loader2.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Loader2 = () => {
   return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
-      <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
-        <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full" />
-      </div>
+    <div className="flex flex-row gap-2">
+      <div className="w-4 h-4 rounded-full bg-green-700 animate-bounce [animation-delay:.7s]"></div>
+      <div className="w-4 h-4 rounded-full bg-green-700 animate-bounce [animation-delay:.3s]"></div>
+      <div className="w-4 h-4 rounded-full bg-green-700 animate-bounce [animation-delay:.7s]"></div>
     </div>
   );
 }
